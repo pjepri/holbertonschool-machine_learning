8bd25fb06030d7588f2dd2d51f60240d6f5bc7a0
Peter Veshi <peterveshi@gmail.com>
2025-11-17 10:00:40 +0100

    Added doc.

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
+    """Calculate the shape of a matrix."""
     shape = []
     current = matrix
     while isinstance(current, list) and len(current) > 0:
