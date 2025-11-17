d3bd24c57afe50bb445c12fd0fc5c38eb9fd274c
Peter Veshi <peterveshi@gmail.com>
2025-11-17 12:40:17 +0100

    pandas/ ex.1

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -10,17 +10,17 @@ import pandas as pd
 def from_numpy(array):
     """
     Creates a pd.DataFrame from a np.ndarray
-    
+
     Args:
         array: np.ndarray from which to create the pd.DataFrame
-        
+
     Returns:
         pd.DataFrame with columns labeled in alphabetical order (A, B, C, ...)
     """
     num_cols = array.shape[1]
-    
-    columns = [chr(65 + i) for i in range(num_cols)]  
-    
+
+    columns = [chr(65 + i) for i in range(num_cols)]
+
     df = pd.DataFrame(array, columns=columns)
-    
+
     return df
